49df19eeb26cd9a1101efdab8254c5abcd8a080b
haproven <haproven.tech@gmail.com>
2026-09-06 19:55:52 +0530

    Rename daly/2026/08/07 page read to daly/2026/08/07 page read/index.html

--- hide.js
+++ hide.js
@@ -1,40 +1,40 @@
-(function () {
+// (function () {
 
-    "use strict";
+//     "use strict";
 
-    /* =========================================================
-       HAPROVEN CORE LOADER
-       Base64 encoded paths
-    ========================================================= */
+//     /* =========================================================
+//        HAPROVEN CORE LOADER
+//        Base64 encoded paths
+//     ========================================================= */
 
-    const decode = function (value) {
-        return atob(value);
-    };
+//     const decode = function (value) {
+//         return atob(value);
+//     };
 
-    const files = [
+//     const files = [
 
-        // auth.js
-        "aHR0cHM6Ly9jb2RlcnN1c2hlZWwuZ2l0aHViLmlvL2hhcHJvYmFzZS9leHRlcm5hbC9qcy9hdXRoL2F1dGguanM=",
+//         // auth.js
+//         "aHR0cHM6Ly9jb2RlcnN1c2hlZWwuZ2l0aHViLmlvL2hhcHJvYmFzZS9leHRlcm5hbC9qcy9hdXRoL2F1dGguanM=",
 
-        // script.js
-        "aHR0cHM6Ly9jb2RlcnN1c2hlZWwuZ2l0aHViLmlvL2hhcHJvYmFzZS9leHRlcm5hbC9qcy9zY3JpcHQuanM=",
+//         // script.js
+//         "aHR0cHM6Ly9jb2RlcnN1c2hlZWwuZ2l0aHViLmlvL2hhcHJvYmFzZS9leHRlcm5hbC9qcy9zY3JpcHQuanM=",
 
-        // page-loader.js
-        "aHR0cHM6Ly9jb2RlcnN1c2hlZWwuZ2l0aHViLmlvL2hhcHJvYmFzZS9leHRlcm5hbC9qcy9wYWdlLWxvYWRlci5qcw==",
+//         // page-loader.js
+//         "aHR0cHM6Ly9jb2RlcnN1c2hlZWwuZ2l0aHViLmlvL2hhcHJvYmFzZS9leHRlcm5hbC9qcy9wYWdlLWxvYWRlci5qcw==",
 
-        // global.js
-        "aHR0cHM6Ly9jb2RlcnN1c2hlZWwuZ2l0aHViLmlvL2hhcHJvYmFzZS9leHRlcm5hbC9qcy9hcGkvZ2xvYmFsLmpz"
+//         // global.js
+//         "aHR0cHM6Ly9jb2RlcnN1c2hlZWwuZ2l0aHViLmlvL2hhcHJvYmFzZS9leHRlcm5hbC9qcy9hcGkvZ2xvYmFsLmpz"
 
-    ];
+//     ];
 
-    files.forEach(function (encoded) {
+//     files.forEach(function (encoded) {
 
-        const src = decode(encoded);
+//         const src = decode(encoded);
 
-        document.write(
-            '<script src="' + src + '"><\/script>'
-        );
+//         document.write(
+//             '<script src="' + src + '"><\/script>'
+//         );
 
-    });
+//     });
 
-})();
+// })();
